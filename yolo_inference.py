577539b1a429a1b3c01b5f463e73e5c8dd848642
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-10-17 20:35:03 +0100

    First usage of Yolov8 model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x.pt')  # Load model
+model = YOLO('yolov8x.pt',device='cuda')  # Load model
 
 results = model.predict('input_videos/08fd33_4.mp4',save=True) # Inference on video, save result to 'runs/detect/exp'
 print(results[0])  # Print results
