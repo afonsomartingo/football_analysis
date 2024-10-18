fdc9fec2771bfb0891a97a155043eeca2a4e85dd
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-10-18 23:58:47 +0100

    Speed and Distance Estimator

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from player_ball_assigner import PlayerBallAssigner
 import numpy as np
 from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 
 def main():
@@ -49,6 +50,10 @@ def main():
     # Interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
+    # Speed and Distance Estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+
     # Assign Player Teams
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
@@ -83,6 +88,9 @@ def main():
     ## Draw camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
 
+    ## Draw speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
+    
     # Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
 
